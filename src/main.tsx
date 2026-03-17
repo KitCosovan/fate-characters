fee935c7ca3c13546212a19b428268a8f1d60007
KitCosovan <cosovankit@gmail.com>
2026-03-17 22:23:58 +0200

    fixed for netlify

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,15 @@ import App from './App'
 import './index.css'
 import './i18n'  // ← инициализация i18n до рендера приложения
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(reg => reg.unregister())
+  })
+  caches.keys().then(keys => {
+    keys.forEach(key => caches.delete(key))
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
